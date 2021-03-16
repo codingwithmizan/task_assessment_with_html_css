f626f7aaa1b5b5b8512098836f22dd2772e6340b
unknown <mizan.samb@gmail.com>
2021-03-16 20:29:50 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xs':'375px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontSize: {
+      'xs': '.65rem',
+      'xss': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '3rem',
+       '6xl': '4rem',
+      '7xl': '5rem',
+     },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
